902663a6a183672239ab75c08eef1447d5e68682
pholpaphankorn <phol12711@outlook.com>
2022-11-28 21:32:48 +0700

    edit try except

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def do_system(arg):
 	err = os.system(arg)
 	if err:
 		print("FATAL: command failed")
-		sys.exit(err)
+		# sys.exit(err)
 
 def testGen3DModel():
     args = parse_args()
@@ -66,46 +66,50 @@ def testGen3DModel():
                     do_system(f'python {run_instant_ngp_file_path} --training_data {folder_path} --mode nerf --save_mesh {output_mesh_file_path}')
                     do_system(f'rm ./data/video/images -r')
                 except:
+                    do_system(f'rm ./data/video/images -r')
                     print("An exception occurred")
         else:
-            base_folder_path='../../3D_Model_Project/'
-            aabb_scale=4
-            if args.fps=='' or args.camm=='' or args.vi=='':
-                print('Please enter video input path or fps or camera model.')
-                return 
-            video_fps_list=int(args.fps)
-            colmap_camera_model_list=args.camm
-            video_path='./data/video/'+args.vi
+            try:
+                base_folder_path='../../3D_Model_Project/'
+                aabb_scale=4
+                if args.fps=='' or args.camm=='' or args.vi=='':
+                    print('Please enter video input path or fps or camera model.')
+                    return 
+                video_fps_list=int(args.fps)
+                colmap_camera_model_list=args.camm
+                video_path='./data/video/'+args.vi
 
- 
-            task_name=f'cup_{str(fps)}_{camera_model}'
-            folder_path=f'./data/{task_name}/'
-            if not os.path.exists(folder_path):
-                os.mkdir(folder_path)
+    
+                task_name=f'cup_{str(fps)}_{camera_model}'
+                folder_path=f'./data/{task_name}/'
+                if not os.path.exists(folder_path):
+                    os.mkdir(folder_path)
 
-            transforms_file_path=folder_path+'transforms.json'
-            video_fps=fps
+                transforms_file_path=folder_path+'transforms.json'
+                video_fps=fps
 
-            colmap_camera_model=camera_model
-            instant_ngp_scripts_folder_path=base_folder_path+'instant-ngp/scripts/'
-            colmap2nerf_file_path=instant_ngp_scripts_folder_path+'colmap2nerf.py'
+                colmap_camera_model=camera_model
+                instant_ngp_scripts_folder_path=base_folder_path+'instant-ngp/scripts/'
+                colmap2nerf_file_path=instant_ngp_scripts_folder_path+'colmap2nerf.py'
 
-            colmap_db_file_path=folder_path+'colmap.db'
-            colmap_text_folder_path=folder_path+'colmap_text'
-
-
-            do_system(f'python {colmap2nerf_file_path} --video_in {video_path} --run_colmap --out {transforms_file_path} --video_fps {video_fps} --aabb_scale {aabb_scale} --colmap_camera_model {colmap_camera_model} --colmap_db {colmap_db_file_path} --text {colmap_text_folder_path}')
+                colmap_db_file_path=folder_path+'colmap.db'
+                colmap_text_folder_path=folder_path+'colmap_text'
 
 
-            colmap_images_folder_path='./data/video/images'
-            rembg_images_folder_path=folder_path+'images_png'
-            do_system(f'rembg p {colmap_images_folder_path} {rembg_images_folder_path}')
-            replaceWordInTransformsJson(transforms_file_path)
-            run_instant_ngp_file_path=instant_ngp_scripts_folder_path+'run.py'
-            output_mesh_file_path=folder_path+f'{task_name}.obj'
-            do_system(f'python {run_instant_ngp_file_path} --training_data {folder_path} --mode nerf --save_mesh {output_mesh_file_path}')
-            do_system(f'rm ./data/video/images -r')
+                do_system(f'python {colmap2nerf_file_path} --video_in {video_path} --run_colmap --out {transforms_file_path} --video_fps {video_fps} --aabb_scale {aabb_scale} --colmap_camera_model {colmap_camera_model} --colmap_db {colmap_db_file_path} --text {colmap_text_folder_path}')
 
+
+                colmap_images_folder_path='./data/video/images'
+                rembg_images_folder_path=folder_path+'images_png'
+                do_system(f'rembg p {colmap_images_folder_path} {rembg_images_folder_path}')
+                replaceWordInTransformsJson(transforms_file_path)
+                run_instant_ngp_file_path=instant_ngp_scripts_folder_path+'run.py'
+                output_mesh_file_path=folder_path+f'{task_name}.obj'
+                do_system(f'python {run_instant_ngp_file_path} --training_data {folder_path} --mode nerf --save_mesh {output_mesh_file_path}')
+                do_system(f'rm ./data/video/images -r')
+            except:
+                do_system(f'rm ./data/video/images -r')
+                print("An exception occurred")
     return 'A 3D Model has been generated.'
 
 
