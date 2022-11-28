4a2b83c66526bc51206633783e703eac52b509e3
pholpaphankorn <phol12711@outlook.com>
2022-11-28 21:27:39 +0700

    added test gen 3d

--- main.py
+++ main.py
@@ -26,51 +26,42 @@ class Generate3DModel(Resource):
         base_folder_path='../../3D_Model_Project/'
         
         aabb_scale=4
-        video_fps_list=[15,30,60]
-        colmap_camera_model_list=["PINHOLE","RADIAL","OPENCV"]
+        fps=7
+        camera_model="RADIAL"
         
         f = request.files['file']
         video_path='./data/video/'+secure_filename(f.filename)
         f.save(video_path)
 
-        for fps in video_fps_list:
-            for camera_model in colmap_camera_model_list:
-                try:
-                    task_name=f'cup_{str(fps)}_{camera_model}'
-                    folder_path=f'./data/{task_name}/'
-                    if not os.path.exists(folder_path):
-                        os.mkdir(folder_path)
+        try:
+            task_name=f'cup_{str(fps)}_{camera_model}'
+            folder_path=f'./data/{task_name}/'
+            if not os.path.exists(folder_path):
+                os.mkdir(folder_path)
 
-        
-                    transforms_file_path=folder_path+'transforms.json'
+            transforms_file_path=folder_path+'transforms.json'
+            video_fps=fps
 
-                    video_fps=fps
-        
-                    colmap_camera_model=camera_model
+            colmap_camera_model=camera_model
+            instant_ngp_scripts_folder_path=base_folder_path+'instant-ngp/scripts/'
+            colmap2nerf_file_path=instant_ngp_scripts_folder_path+'colmap2nerf.py'
+    
+            colmap_db_file_path=folder_path+'colmap.db'
+            colmap_text_folder_path=folder_path+'colmap_text'
 
-                    instant_ngp_scripts_folder_path=base_folder_path+'instant-ngp/scripts/'
 
-                    colmap2nerf_file_path=instant_ngp_scripts_folder_path+'colmap2nerf.py'
-            
-                    colmap_db_file_path=folder_path+'colmap.db'
-                    colmap_text_folder_path=folder_path+'colmap_text'
-        
-        
-                    do_system(f'python {colmap2nerf_file_path} --video_in {video_path} --run_colmap --out {transforms_file_path} --video_fps {video_fps} --aabb_scale {aabb_scale} --colmap_camera_model {colmap_camera_model} --colmap_db {colmap_db_file_path} --text {colmap_text_folder_path}')
-      
-      
-                    colmap_images_folder_path='./data/video/images'
-                    rembg_images_folder_path=folder_path+'images_png'
-                    do_system(f'rembg p {colmap_images_folder_path} {rembg_images_folder_path}')
+            do_system(f'python {colmap2nerf_file_path} --video_in {video_path} --run_colmap --out {transforms_file_path} --video_fps {video_fps} --aabb_scale {aabb_scale} --colmap_camera_model {colmap_camera_model} --colmap_db {colmap_db_file_path} --text {colmap_text_folder_path}')
 
-                    replaceWordInTransformsJson(transforms_file_path)
-
-                    run_instant_ngp_file_path=instant_ngp_scripts_folder_path+'run.py'
-                    output_mesh_file_path=folder_path+f'{task_name}.obj'
-                    do_system(f'python {run_instant_ngp_file_path} --training_data {folder_path} --mode nerf --save_mesh {output_mesh_file_path}')
-                    do_system(f'rm ./data/video/images -r')
-                except:
-                    print("An exception occurred")
+            colmap_images_folder_path='./data/video/images'
+            rembg_images_folder_path=folder_path+'images_png'
+            do_system(f'rembg p {colmap_images_folder_path} {rembg_images_folder_path}')
+            replaceWordInTransformsJson(transforms_file_path)
+            run_instant_ngp_file_path=instant_ngp_scripts_folder_path+'run.py'
+            output_mesh_file_path=folder_path+f'{task_name}.obj'
+            do_system(f'python {run_instant_ngp_file_path} --training_data {folder_path} --mode nerf --save_mesh {output_mesh_file_path}')
+            do_system(f'rm ./data/video/images -r')
+        except:
+            print("An exception occurred")
       
         return 'A 3D Model has been generated.'
 
