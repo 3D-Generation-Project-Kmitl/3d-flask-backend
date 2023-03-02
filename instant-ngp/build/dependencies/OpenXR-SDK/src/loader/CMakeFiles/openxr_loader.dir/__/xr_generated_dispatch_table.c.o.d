e4c055932f8c848003858c38c31000da24cedc47
pholpaphankorn <phol12711@outlook.com>
2023-03-02 18:03:01 +0700

    add build

---
+++ instant-ngp/build/dependencies/OpenXR-SDK/src/loader/CMakeFiles/openxr_loader.dir/__/xr_generated_dispatch_table.c.o.d
@@ -0,0 +1,49 @@
+dependencies/OpenXR-SDK/src/loader/CMakeFiles/openxr_loader.dir/__/xr_generated_dispatch_table.c.o: \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/xr_generated_dispatch_table.c \
+ /usr/include/stdc-predef.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/xr_generated_dispatch_table.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/common/xr_dependencies.h \
+ /usr/include/X11/Xlib.h /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/X11/X.h /usr/include/X11/Xfuncproto.h \
+ /usr/include/X11/Xosdefs.h /usr/include/X11/Xutil.h \
+ /usr/include/X11/keysym.h /usr/include/X11/keysymdef.h \
+ /usr/include/GL/glx.h /usr/include/GL/gl.h /usr/include/GL/glext.h \
+ /usr/include/KHR/khrplatform.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/GL/glxext.h /usr/include/inttypes.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/../include/openxr/openxr.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/../include/openxr/openxr_platform_defines.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/../include/openxr/openxr_platform.h \
+ /volume/instant-ngp/dependencies/OpenXR-SDK/src/../include/openxr/openxr.h
