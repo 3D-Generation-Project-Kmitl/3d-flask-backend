6e7cabe5caff2850f090aafb80a6c4df116187a5
pholpaphankorn <phol12711@outlook.com>
2023-03-17 23:31:00 +0700

    removed image

--- instant-ngp/build/dependencies/OpenXR-SDK/src/loader/CMakeFiles/openxr_loader.dir/android_utilities.cpp.o.d
+++
@@ -1,4 +0,0 @@
-dependencies/OpenXR-SDK/src/loader/CMakeFiles/openxr_loader.dir/android_utilities.cpp.o: \
- /volume/instant-ngp/dependencies/OpenXR-SDK/src/loader/android_utilities.cpp \
- /usr/include/stdc-predef.h \
- /volume/instant-ngp/dependencies/OpenXR-SDK/src/loader/android_utilities.h
