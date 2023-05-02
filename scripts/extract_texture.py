00d31b80cebc21a590c2fed36cc1d7a54ca87a9a
pholpaphankorn <phol12711@outlook.com>
2023-05-02 13:14:13 +0700

    move scripts to scripts folder

--- scripts/extract_texture.py
+++ scripts/extract_texture.py
@@ -8,13 +8,13 @@ import numpy as np
 
 
 texture_res_threshold=16384
-texture_res=4096
-texture_dimension=4096
-task_name="111_4_37_real_esrgan_poisson_01_Low"
-mesh_file_name="111_4_37_real_esrgan_poisson_01_Low"
+texture_res=256
+texture_dimension=256
+task_name="3_78"
+mesh_file_name="3_78"
 input_mesh=f"data/{task_name}/{mesh_file_name}.ply"
 # input_poisson_mesh=f"data/{task_name}/{mesh_file_name}_poisson_scale_0.3.ply"
-ext_name=f'_pymeshlab_textdim_{texture_dimension}_texres_{texture_res}_scale_0.5'
+ext_name=f'_pymeshlab_textdim_{texture_dimension}_texres_{texture_res}_low_tex'
 # depth=9
 output=f"data/{task_name}/{mesh_file_name}{ext_name}.ply"
 output_mesh_file_path_glb=f"data/{task_name}/{mesh_file_name}{ext_name}.glb"
@@ -54,27 +54,27 @@ import open3d as o3d
 # pcd=o3d.io.read_point_cloud(input_mesh)
 # mesh,densities=o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=depth)
 # o3d.io.write_triangle_mesh(input_poisson_mesh, mesh)
-# ms = pymeshlab.MeshSet()
-# ms.load_new_mesh(input_mesh)
-# # scene = a3d.Scene.from_file(input_poisson_mesh)
-# # scene.save(output_mesh_file_path_glb)
+
+ms = pymeshlab.MeshSet()
+ms.load_new_mesh(input_mesh)
+# scene = a3d.Scene.from_file(input_poisson_mesh)
+# scene.save(output_mesh_file_path_glb)
 # ms.generate_surface_reconstruction_screened_poisson()
 # ms.save_current_mesh(output)
 
-# while texture_res<texture_res_threshold:
-#     try:
-#         ms.compute_texcoord_parametrization_triangle_trivial_per_wedge(textdim=texture_dimension)
+while texture_res<texture_res_threshold:
+    try:
+        ms.compute_texcoord_parametrization_triangle_trivial_per_wedge(textdim=texture_dimension)
 
-#         # # create texture using UV map and vertex colors
-#         ms.compute_texmap_from_color(textname=f"{mesh_file_name}{ext_name}",textw=texture_res,texth=texture_res) # textname will be filename of a png, should not be a full path
-#         # texture file won't be saved until you save the mesh
-#         ms.save_current_mesh(output)
-#         scene = a3d.Scene.from_file(output)
-#         scene.save(output_mesh_file_path_glb)
-#         break
-#     except:
-#         texture_res*=2
-#         texture_dimension*=2
+        # # create texture using UV map and vertex colors
+        ms.compute_texmap_from_color(textname=f"{mesh_file_name}{ext_name}",textw=texture_res,texth=texture_res) # textname will be filename of a png, should not be a full path
+        # texture file won't be saved until you save the mesh
+        ms.save_current_mesh(output)
+        scene = a3d.Scene.from_file(output)
+        scene.save(output_mesh_file_path_glb)
+        break
+    except:
+        texture_dimension*=2
 
 
 # pcd=o3d.io.read_point_cloud(input_mesh)
